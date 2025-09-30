51a4804b2dd56d25a91d8f1d5ec99570a022e49c
Gabriel Brevilieri <gbrevilieri@gmail.com>
2025-09-29 22:07:52 -0300

    sync

--- run.sh
+++ run.sh
@@ -2,9 +2,10 @@
 
 # $1 = caminho do arquivo .c
 # $2 = número de processos (opcional, default = 4)
+# $3... = argumentos extras para o programa
 
 if [ -z "$1" ]; then
-  echo "Uso: $0 arquivo.c [num_processos]"
+  echo "Uso: $0 arquivo.c [num_processos] [args...]"
   exit 1
 fi
 
@@ -12,7 +13,12 @@ mkdir -p output
 
 FILE=$1
 NP=${2:-4}
+
+# Pega os argumentos extras (a partir do 3º argumento)
+shift 2
+ARGS="$@"
+
 OUT="output/$(basename "$FILE" .c)"
 
 mpicc "$FILE" -o "$OUT"
-mpirun -np "$NP" "$OUT"
+mpirun -np "$NP" "$OUT" $ARGS
